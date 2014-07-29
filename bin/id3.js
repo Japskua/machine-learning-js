353a08ad813c9a028baad0df5dd98f280f1f62b7
Janne Parkkila <japskua@gmail.com>
2014-07-29 12:00:20 +0300

    Started creating tests for the id3 tree. Now bugs for some reason at looping through the nodes. should be fixed later.

--- bin/id3.js
+++ bin/id3.js
@@ -2,19 +2,108 @@
  * Created by Janne on 28.7.2014.
  */
 
-"use strict";
-
 var  _ = require('underscore');
 var math = require('mathjs');
 var helpers = require('./helpers.js');
 
 
+/**
+ * The ID3 Decission Tree algorithm for classifying values
+ * @param {List} data The dataset to classify
+ * @param {String} target The target to search the match for
+ * @param {List} features List of features used in the decision making process
+ * @constructor
+ */
 function ID3 (data, target, features) {
+    if ((data === null) || (data.length === 0) || (data === undefined)) {
+        throw new Error("The dataset is empty");
+    }
+    if ((target === null) || (target === undefined)) {
+        throw new Error("The target is missing!");
+    }
+    if ((features === null) || (features.length === 0) || (features === undefined)) {
+        throw new Error("The features list is empty");
+    }
+    // Get the data, target and features
     this.data = data;
     this.target = target;
     this.features = features;
+    // Then, construct the tree
+    this.model = helpers.createTree(data, target, features);
 
-    console.log("Done!");
 }
+
+
+ID3.prototype = {
+
+    /**
+     * Predicts how the given sample should be classified
+     * @param {String} sample The sample value to classify
+     * @returns {String} The value to which class it belongs to
+     */
+    predict : function (sample) {
+        // Make the model to be root
+        var node = this.model;
+
+        // Then, loop until you find the result node
+        while (node.type !== "result") {
+            // Get the attribute name
+            var attribute = node.name;
+            // Get the sample value of the node
+            var sampleValue = sample[attribute];
+            // Find the child node
+            var childNode = _.find(node.values, function (x) {
+                // And return the child node matching the sample value
+                return x.name == sampleValue;
+            });
+            // And set the child to be the new "root"
+            node = childNode.child;
+        }
+
+        // Once we hit the root.type === "result"
+        // Return the value
+        return node.value;
+
+    }, // End of predict()
+
+    /**
+     * Evaluates the correctness of the decission tree
+     * @param {List} samples List of samples to use with the Decission tree
+     * @returns {number} The classification percentage of correct/total
+     */
+    evaluate : function (samples) {
+        // Set this to be the instance
+        var instance = this;
+        // And get the target
+        var target = this.target;
+
+        // Amount of samples gone through
+        var total = 0;
+        // Amount of correct answers predicted
+        var correct = 0;
+
+        // loop through all the samples
+        _.each(samples, function(sample) {
+            // Increment the amount of samples processed
+            total++;
+
+            // Predict the class for the sample
+            var prediction = instance.predict(sample);
+            // Get the actual class for the sample
+            var actual = sample[target];
+            // Compare the two
+            if (prediction === actual) {
+                // One more correct!
+                correct++;
+            }
+        }); // End of _.each(...)
+
+        // Return the correct ratio
+        return correct / total;
+
+    } // End of evaluate()
+
+};
+
 
 module.exports = ID3;
--- test/id3-test.js
+++ test/id3-test.js
@@ -3,16 +3,66 @@
  */
 
 var assert = require("assert");
+var expect = require("expect.js");
 var ID3 = require('./../bin/id3.js');
+var _ = require("underscore");
 
 describe("ID3", function() {
 
 
     describe("Creating tree", function() {
 
+        it("Should throw some errors, as there are faulty input values", function() {
+
+            // Faulty constructors
+            expect( function() {
+                new ID3() })
+                .to.throwError(new Error());
+
+            expect( function() {
+                new ID3(["a", "b", "ab", "a"]) })
+                .to.throwError(new Error());
+
+            expect( function() {
+                new ID3(["a", "b", "ab", "a"], "a") })
+                .to.throwError(new Error());
+
+            expect( function() {
+                new ID3(["a", "b", "ab", "a"], null, ["a"]) })
+                .to.throwError(new Error());
+        });
+
+        var features = ["color", "shape"];
+        var dataSet =
+            [
+                {"color":"blue", "shape":"square", "liked":false},
+                {"color":"red", "shape":"square", "liked":false},
+                {"color":"blue", "shape":"circle", "liked":true},
+                {"color":"red", "shape":"circle", "liked":true},
+                {"color":"blue", "shape":"hexagon", "liked":false},
+                {"color":"red", "shape":"hexagon", "liked":false},
+                {"color":"yellow", "shape":"hexagon", "liked":true},
+                {"color":"yellow", "shape":"circle", "liked":true}
+
+            ];
+
         it("Should return true when things start correctly", function() {
 
-            var id3 = new ID3();
+            // Create a new ID3 tree
+            var id3 = new ID3(dataSet, "liked", features);
+
+            // Check that the tree is okay
+            expect(id3).to.ok();
+
+            // Check that the model is okay
+            expect(id3.model).to.ok();
+
+            // Predict a sample data
+            var sample = dataSet[0];
+            console.log("Starting to predict", sample);
+            var predictedClass = id3.predict(sample);
+            //var actualClass = dataSet["liked"];
+            //expect(predictedClass).to.eql(actualClass);
 
 
         });
